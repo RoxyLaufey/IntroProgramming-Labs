3935c6f53daf18aa0bd8c99c26b5e69378d4eb61
Roxanne Lai <roxanne.lai1@marist.edu>
2018-09-06 15:50:07 -0400

    solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,20 @@
+# Author: Roxanne Lai 9/6/18
+# CMPT 120L 113
+# A program that incorporates input and output statements, variable assignment, and string concatenation.
+
+def main():
+    print("Mad Libs")
+    print()
+    print("please enter a word for each part of speech")
+    adjective = input("Adjective: ")
+    noun = input("Noun: ")
+    verb = input("Verb: ")
+    place = input("Place: ")
+
+    print("Once upon a time there was a", adjective, noun, ". It was", verb, "to get to the", place, "for a party. In the end the", noun, "made it on time!")
+main()
+
+
+     
+ 
+    
