e8c8cc84c6d8806d32518ba8a4315e4523974a3c
Roxanne Lai <roxanne.lai1@marist.edu>
2018-09-13 12:46:41 -0400

    added game starter code

---
+++ GameRoxy.py
@@ -0,0 +1,88 @@
+# CMPT 120L 113
+# Matthew A Johnson
+# 10 Sep 2018
+###
+
+# show game title
+print("\nSTRANDED"
+      "\n========\n")
+
+# show game introduction
+print("You have been training for this moment for years. Countless hours"
+      " studying the maps, practicing off the coast whenever the seas were"
+      " less than placid to prepare yourself for the worst eventualities."
+      " Now, the moment has arrived, and you embark of this voyage full of"
+      " anticipation. Sailing into one of the last unexplored regions of"
+      " the Pacific Ocean, you feel certain that you will find something"
+      " no one else has seen, and the history books will list your name"
+      " among the great explorers of the modern age. On the third night of"
+      " your journey, a torrential rain begins to fall and the winds howl"
+      " and swirls around you and you vessel. You draw upon all your"
+      " training and preparation, but to no avail... your boat capsizes"
+      " and dumps you violently into the dark, raging waters. An inky"
+      " blackness swiftly envelopes you, and icy chill spreads throughout"
+      " your body...")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You awake to find yourself on an empty beach. "
+          "You see various debris strewn along the shoreline. "
+          "The jungle looms a short distance away.")
+
+# show the current score
+print("Your score is 0.")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You walk into the dark jungle, where the air is damp and thick. "
+      "Unfamiliar noises only add to you feeling of unease. "
+      "You can make out a faint trail winding through the trees.")
+
+# show the current score
+print("Your score is 5.")
+
+# same as above
+input("\n<Press Enter to continue...>\n")
+
+# same as above
+print("The path leads to a small, decrepit thatch hut."
+      "The entrance to the structure is clear; what door there might"
+      "once have been is long gone.")
+
+# same as above
+print("Your score is 10.")
+
+# again...
+input("\n<Press Enter to continue...>\n")
+
+# again...
+print("A short distance beyond the hut towers an impressive cataract. "
+      "The rushing of the water drowns out all other sounds, "
+      "and you feel immediately at peace.")
+
+# again...
+print("Your score is 15.")
+
+# yet again...
+input("\n<Press Enter to continue...>\n")
+
+# yet again...
+print("You squeeze between the wet rock behind the waterfall and "
+      "discover a large cave.")
+
+# yet again...
+print("Your score is 20.")
+
+# one last time...
+input("\n<Press Enter to continue...>\n")
+
+# show game ending
+print("Suddenly, the crack of a twig behind you is the only warning you "
+      "have before being mauled and eaten by a hungry beast. Game over!")
+
+# show credits
+print("\nCopyright (c) 2016-2018 Matthew A Johnson, Matt.Johnson@marist.edu")
