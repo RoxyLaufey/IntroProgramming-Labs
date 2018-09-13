40d4f64ed593cf63fccc5f12caca99b103568f48
RoxyLaufey <42847168+RoxyLaufey@users.noreply.github.com>
2018-09-13 15:30:11 -0400

    Update evens.py

--- evens.py
+++ evens.py
@@ -2,9 +2,7 @@
 # CMPT 120L 113
 # A program that uses a for loop - printing all even intergers between 2 and 20
 
-def main():
-    for i in range(2,21):
+for i in range(2,21):
         if i%2 == 0:
             print(i)
-main()
-    
+
