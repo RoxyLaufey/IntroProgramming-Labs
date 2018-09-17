02edea716517c4543c6b6a097eaa52e669e8798c
RoxyLaufey <42847168+RoxyLaufey@users.noreply.github.com>
2018-09-17 14:19:02 -0400

    Delete evens.py

--- evens.py
+++
@@ -1,8 +0,0 @@
-# Author: Roxanne Lai 9/6/18
-# CMPT 120L 113
-# A program that uses a for loop - printing all even intergers between 2 and 20
-main():
-for i in range(2,21):
-        if i%2 == 0:
-            print(i)
-main()
