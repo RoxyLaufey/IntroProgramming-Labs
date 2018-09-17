b57dc6db14cd190145b401b18d7e0097a846e0d8
RoxyLaufey <42847168+RoxyLaufey@users.noreply.github.com>
2018-09-17 14:13:09 -0400

    Update and rename Example.py to Hello.py

--- Example.py
+++
@@ -1,6 +0,0 @@
-# Example.py
-# Example for class 9/6/18
-# Author: Roxanne Lai
-
-print("Hello, World!")
-
---
+++ Hello.py
@@ -0,0 +1,6 @@
+# Introduction to programming
+# Date: 9/6/18
+# Author: Roxanne Lai
+def main():
+  print("Hello, instructor!")
+main()
