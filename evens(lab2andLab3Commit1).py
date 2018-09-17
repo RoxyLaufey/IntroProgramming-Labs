e12861296c25b4c0334e9de36467c851c0da3a22
RoxyLaufey <42847168+RoxyLaufey@users.noreply.github.com>
2018-09-17 15:31:05 -0400

    Update evens(lab2andLab3Commit1).py

--- evens(lab2andLab3Commit1).py
+++ evens(lab2andLab3Commit1).py
@@ -4,8 +4,13 @@
 # Date: 9/6/18
 # A program that uses a for loop - printing all even integers between 2 and 20
 
-for i in range(2,21):
+
+def main():
+    for i in range(2,21):
         if i % 2 == 0:
             print(i)
         else:
             print("something went wrong")
+
+
+main()
