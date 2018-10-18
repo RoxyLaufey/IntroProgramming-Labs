4c6b1150642cb536b396c4838e15966205d21e64
Roxanne Lai <roxanne.lai1@marist.edu>
2018-10-18 15:15:01 -0400

    Usability improvements to the guessing game, resolves #4 and resolves #5.

--- guessing-game.py
+++ guessing-game.py
@@ -8,19 +8,25 @@ def main():
     print("GAME FINISHED")
 answer = "cat"
 guess = "dog"
-
+getOut = "quit"
 
 while not guess == answer:
     print("PYTHON GUESSING GAME.")
     print("I am thinking of an animal...")
     guess = str(input("guess the name of the animal: "))
     userGuess = guess.lower().strip()
-    if userGuess == answer:
-        print("you guessed correctly, you win!")
+    if userGuess == getOut[0]:
+        print("sorry this game is too hard for you!")
         break
-    elif userGuess == "quit":
-        print("sorry this guessing game is too hard for you!")
-        break
+    elif userGuess == answer:
+        print("you guessed correctly, congrats!")
+        feeling = str(input("do you like the animal? type yes or no: "))
+        if feeling == "yes":
+            print("I like cats too!")
+            break
+        else:
+            print("You are so mean, cats are amazing!")
+            break
     else:
         print("You did not guess the right animal, guess again")
 
